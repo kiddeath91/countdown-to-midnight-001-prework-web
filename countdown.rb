be3638bd562e53c3e50dbaa1a13e96ccc5fc042d
kiddeath91 <jason@laskodi.com>
2016-02-13 21:07:41 -0500

    Countdown to Midnight

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  while int != 0
+  puts "#{int} SECOND(S)!"
+  int -= 1
+  break if int == 0
 end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  sleep(10)
+#  while int != 0
+#  int -= 1
+#  break if int == 0
+#end
+end
+countdown(25)
+countdown_with_sleep(25)
